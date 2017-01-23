5644e4e3de8b11a2372de8c1f5c6e0b0c03e75af
clear ls <ls>
2017-01-22 18:59:10 -0800

    working on portfolio pages in place

--- js/main.js
+++ js/main.js
@@ -1,65 +1,14 @@
-
-
-// responsive nav bar
-var mobile = document.createElement('div');
-mobile.className = 'nav-mobile';
-document.querySelector('.nav').appendChild(mobile);
-
-var mobileNav = document.querySelector('.nav-mobile');
-var toggle = document.querySelector('.nav-list');
-mobileNav.onclick = function() {
-  toggleClass(this, 'nav-mobile-open');
-  toggleClass(toggle, 'nav-active');
-}
-
-// hasClass
-function hasClass(elem, className) {
-  return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
-}
-
-// toggleClass
-function toggleClass(elem, className) {
-  var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, ' ') + ' ';
-    if (hasClass(elem, className)) {
-        while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
-            newClass = newClass.replace(' ' + className + ' ', ' ');
-        }
-        elem.className = newClass.replace(/^\s+|\s+$/g, '');
-    } else {
-        elem.className += ' ' + className;
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
     }
-}
-
-(function () {
-
-    // Create mobile element
-    var mobile = document.createElement('div');
-    mobile.className = 'nav-mobile';
-    document.querySelector('.nav').appendChild(mobile);
-
-    // hasClass
-    function hasClass(elem, className) {
-        return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
-    }
-
-    // toggleClass
-    function toggleClass(elem, className) {
-        var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, ' ') + ' ';
-        if (hasClass(elem, className)) {
-            while (newClass.indexOf(' ' + className + ' ') >= 0) {
-                newClass = newClass.replace(' ' + className + ' ', ' ');
-            }
-            elem.className = newClass.replace(/^\s+|\s+$/g, '');
-        } else {
-            elem.className += ' ' + className;
-        }
-    }
-
-    // Mobile nav function
-    var mobileNav = document.querySelector('.nav-mobile');
-    var toggle = document.querySelector('.nav-list');
-    mobileNav.onclick = function () {
-        toggleClass(this, 'nav-mobile-open');
-        toggleClass(toggle, 'nav-active');
-    };
-})();
+  });
+});
